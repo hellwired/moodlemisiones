e278a9411a35f0f19d6bd3cef3d00060cafbdb79
hellwired <lexclaudio@gmail.com>
2024-08-09 12:59:20 -0300

    Version 0.01 moodleMisiones

---
+++ local/kopere_dashboard/amd/src/backup.js
@@ -0,0 +1,10 @@
+define(["jquery"], function($) {
+    return {
+
+        backup_animate_scrollTop : function(key) {
+            jQuery('html,body').animate({scrollTop : $(key).offset().top}, 0);
+        },
+
+    };
+});
+
